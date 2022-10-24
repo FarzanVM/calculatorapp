a3e913b09b4c78312d68e317178c75a65cc9e1c7
FarzanVM <94286246+FarzanVM@users.noreply.github.com>
2022-10-24 21:50:37 +0530

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,15 @@
+package jenkins;
+
+public class calculator {
+	public static void main(String args[]){
+		int a=20;
+		int b=10;
+		int mul= a*b;
+		int div= a/b;
+		
+		System.out.println("Multiplication result "+mul);
+		System.out.println("Division result "+div);
+		
+	}
+
+}
